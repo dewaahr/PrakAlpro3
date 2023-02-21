a3368f06ab4a387c5a6f295b9cc903720fc07867
darkoboy <118077486+dewaahr@users.noreply.github.com>
2023-02-21 12:25:32 +0700

    Add files via upload

---
+++ prakalpro3.py
@@ -0,0 +1,67 @@
+# # suhu_badan=float(input('Masukan suhu badan:'))
+# # if suhu_badan<32:
+# #     print("ded")
+# # elif 32<suhu_badan<=36:
+# #     print("Hipotermia")
+# # elif 36<suhu_badan<=38:
+# #     print("normal")
+# # elif 38<suhu_badan<=41:
+# #     print("demam")
+# # else:print("meninggoy")
+
+
+# # while True:
+# #     angka=int(input("Masukan angka:"))
+# #     if angka>0:
+# #         print("positif")
+# #     elif angka<0:
+# #         print("negatif")
+# #     else:print("bilangan nol")
+
+
+# a = int(input("Masukkan bilangan pertama: "))
+# b = int(input("Masukkan bilangan kedua: "))
+# c = int(input("Masukkan bilangan ketiga: "))
+# if a > b and a > c:
+#     print("Terbesar: ", a)
+# elif b > a and b > c:
+#     print("Terbesar: ", b)
+# elif c > a and c > b:
+#     print("Terbesar: ", c)
+
+
+# a = int(input("Masukkan bilangan pertama: "))
+# a=a%10
+
+# if a==1 or a==2 or a==8 or a==9 or a==0:
+#     print("true")
+# else : print("false")
+
+
+# a = str(input("pertama: "))
+# b = str(input("kedua: "))
+# c = str(input("ketiga: "))
+# if len(a) > len(b) and len(a) > len(c):
+#     print("terpanjang: ", a)
+# elif len(b) > len(a) and len(b) > len(c):
+#     print("terpanjang: ", b)
+# elif len(c) > len(a) and len(c) >len(b):
+#     print("terpanjang: ", c)
+
+# a = int(input("pertama: "))
+# b = int(input("kedua: "))
+# c = int(input("ketiga: "))
+# total=0
+# if a!=b !=c and b!=c:
+#     total=a+b+c
+    
+# elif a==b and a==c and b==c:
+#     total=0
+# elif a==b:
+#     total=c
+# elif a==c:
+#     total=b
+# else:
+#     total=a
+
+# print(total)
